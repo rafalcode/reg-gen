1115e2f4c54e571ece1f65601a7ffa698b251d58
lzj1769 <lzj1769@gmail.com>
2017-06-11 15:58:53 +0200

    fix

--- rgt/HINT/signalProcessing.py
+++ rgt/HINT/signalProcessing.py
@@ -297,9 +297,9 @@ class GenomicSignal:
         for i in range((window / 2), len(af) - (window / 2)):
             nhatf = Nf[i - (window / 2)] * (af[i] / fSum)
             nhatr = Nr[i - (window / 2)] * (ar[i] / rSum)
-            zf = log(nf[i] + 1) - log(nhatf + 1)
-            zr = log(nr[i] + 1) - log(nhatr + 1)
-            bias_corrected_signal.append(zf + zr)
+            #zf = log(nf[i] + 1) - log(nhatf + 1)
+            #zr = log(nr[i] + 1) - log(nhatr + 1)
+            bias_corrected_signal.append(nhatf + nhatr)
             fSum -= fLast
             fSum += af[i + (window / 2)]
             fLast = af[i - (window / 2) + 1]
